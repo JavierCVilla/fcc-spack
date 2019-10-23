8ef76aa0884d85757eb0ffdce5d7d4c43850d422
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2019-10-23 09:34:54 +0200

    Point to the AIDASoft repo for newer podio versions

--- packages/podio/package.py
+++ packages/podio/package.py
@@ -29,14 +29,14 @@ class Podio(CMakePackage):
     """PODIO, or plain-old-data I/O, is a C++ library to support the creation
     and handling of data models in particle physics."""
 
-    homepage = "https://github.com/HEP-FCC/podio"
-    url      = "https://github.com/HEP-FCC/podio/archive/v0.9.1.tar.gz"
+    homepage = "https://github.com/AIDASoft/podio"
 
-    version('0.9.1', '9e2b8cf6d2be46d74471ae4857bd7191', preferred=True)
+    version('00.09.02', sha1='5fe64ec87ffa62e225edffc6532d2cc0b139b1f3', preferred=True)
+    version('0.9.1', '9e2b8cf6d2be46d74471ae4857bd7191')
     version('0.8', '07bf090649fc8e3b94e348e6b7fb8c7e')
     version('0.7', '79e11c02c8d588f7b1dfc39b7de4eed9')
     version('0.6', '1a1d2aa70fc16cce372ce10c3612c32d')
-    version('develop', git='https://github.com/HEP-FCC/podio.git', branch='master')
+    version('develop', git='https://github.com/AIDASoft/podio.git', branch='master')
 
     variant('build_type', default='Release',
             description='The build type to build',
@@ -60,3 +60,11 @@ class Podio(CMakePackage):
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('PODIO', self.prefix)
+
+    def url_for_version(self, version):
+	old_url = "https://github.com/hep-fcc/podio/archive/v{}.tar.gz"
+	new_url = "https://github.com/AIDASoft/podio/archive/v{}.tar.gz"
+	if version <= Version('0.9.1'):
+	    return old_url.format(version)
+	else:
+	    return new_url.format(version.dashed)
